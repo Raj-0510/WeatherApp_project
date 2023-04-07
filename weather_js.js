ad831b3a9b1394f617fe51d4faa40e21380d74fd
Raj Jayswal <rajjayswal480@gmail.com>
2023-04-07 17:45:36 +0530

    3rd commit

--- weather_js.js
+++ weather_js.js
@@ -11,7 +11,6 @@ oldTab=userTab;
 const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 oldTab.classList.add("current-tab");
 grantAccessContainer.classList.add("active");
-console.log("Yes boy")
 function switchTab(newTab){
     if(newTab!=oldTab){
     oldTab.classList.remove("current-tab");
